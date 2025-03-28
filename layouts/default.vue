861a21320ab85d7dc4c8e7da85a256e9337c4f4a
Cristian Camargo <contato@camargodev.com.br>
2025-03-28 17:29:21 -0300

    feat: add google tag

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,6 +6,11 @@
         <slot/>
     </main>
     <Footer/>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript>
+      <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TFFM6HSM" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    </noscript>
+    <!-- End Google Tag Manager (noscript) -->
 </template>
 
 <script setup lang="ts">
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   components: [{ path: '~/components', pathPrefix: false }],
   css: ['@/assets/css/main.css', '@/assets/css/hamburguer.css'],
-  modules: ['@nuxt/image', 'nuxt-swiper', '@pinia/nuxt', "@nuxtjs/i18n", 'nuxt-gtag'],
+  modules: ['@nuxt/image', 'nuxt-swiper', '@pinia/nuxt', "@nuxtjs/i18n"],
   postcss: {
     plugins: {
       tailwindcss: {},
@@ -14,15 +14,23 @@ export default defineNuxtConfig({
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
-      title: 'RM AGRO | Consultoria e Gestão de Risco',
-    }
+      title: 'RM AGRO | Consultoria e Gestão de Risco', 
+      script:  [
+        {
+          hid: 'gtm',
+          innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-TFFM6HSM');`,
+          type: 'text/javascript',
+        },
+      ],
+      
+    },
   },
   i18n: {
       vueI18n: '@/i18n/i18n.config.ts',
       defaultLocale: 'pt',
   },
-  gtag: {
-    enabled: process.env.NODE_ENV === 'production',
-    id: 'GTM-TFFM6HSM'
-  }
 })
